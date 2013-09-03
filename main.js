dd6c0b2483e437abc5f3bd196bd6173d6cbf751e
pix <thatpixguy@gmail.com>
2013-09-03 22:43:39 +0930

    maximise

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var blockOffset = 16;
 var Q = Quintus({ development: true })                          // Create a new engine instance
   .include("Sprites, Scenes, Input, 2D, Touch, UI") // Load any needed modules
 
-  Q.setup()                           // Add a canvas element onto the page
+  Q.setup({maximize: true})                           // Add a canvas element onto the page
    .controls()                        // Add in default controls (keyboard, buttons)
    .touch(Q.SPRITE_ALL);                          // Add in touch support (for the UI)
 
